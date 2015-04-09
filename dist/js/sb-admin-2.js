486c11882b092ac01f6d94b25b1bd5e827c949c9
Ivan Kurnosov <zerkms@zerkms.com>
2015-04-10 08:43:29 +1200

    All local variables must be prepended with `var`
    Otherwise it causes problems in `'use strict';` mode

--- dist/js/sb-admin-2.js
+++ dist/js/sb-admin-2.js
@@ -9,8 +9,8 @@ $(function() {
 // Sets the min-height of #page-wrapper to window size
 $(function() {
     $(window).bind("load resize", function() {
-        topOffset = 50;
-        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        var topOffset = 50;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
             $('div.navbar-collapse').addClass('collapse');
             topOffset = 100; // 2-row-menu
@@ -18,7 +18,7 @@ $(function() {
             $('div.navbar-collapse').removeClass('collapse');
         }
 
-        height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
         height = height - topOffset;
         if (height < 1) height = 1;
         if (height > topOffset) {
