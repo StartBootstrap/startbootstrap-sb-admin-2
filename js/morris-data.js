ee4a72ad9798544b91c4d2f5e50726e545759cee
Marcelo Diniz <marcelo.leo27@gmail.com>
2015-06-02 15:47:55 -0300

    add exemple morris-line-chart

--- js/morris-data.js
+++ js/morris-data.js
@@ -76,6 +76,23 @@ $(function() {
         resize: true
     });
 
+    // http://jsbin.com/uqawig/441/embed?js,output
+    Morris.Line({
+      element: 'morris-line-chart',
+      data: [
+        { y: '2006', a: 100, b: 90 },
+        { y: '2007', a: 75,  b: 65 },
+        { y: '2008', a: 50,  b: 40 },
+        { y: '2009', a: 75,  b: 65 },
+        { y: '2010', a: 50,  b: 40 },
+        { y: '2011', a: 75,  b: 65 },
+        { y: '2012', a: 100, b: 90 }
+      ],
+      xkey: 'y',
+      ykeys: ['a', 'b'],
+      labels: ['Series A', 'Series B']
+    });
+
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
