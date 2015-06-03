6b93553795b0fd07a02511a20ae403b450967155
Alfonso Aranzazu <aaranzaz@uci.edu>
2015-06-03 16:58:54 -0700

    3D Visualization shows the cube, importing data now works.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var importedData = new Array();
-// var dataStream1 = document.getElementById("2DdataStream1");
+var importedDataSize = importedData.length;
+var isDataImported = false;
 var HeatMapData = new Array();
+var outputPath3D = null;
 
+
+// Array recording which operators have been processed
+// index 0 = filter
+// index 1 = group
+// index 2 = aggregate
+// index 3 = 2D
+// index 4 = 3D
 var processedOperators = new Array();
 processedOperators.push(false);
 processedOperators.push(false);
@@ -9,23 +18,29 @@ processedOperators.push(false);
 processedOperators.push(false);
 processedOperators.push(false);
 
-
-
-
 function loadDataStreams(){
-	// alert(dataStream1);
-	for (var i = 0; i < importedData.length; i++) {
-		var filterOption = document.createElement("option");
-	   	var filterText = document.createTextNode(importedData[i]);
-	   	filterOption.appendChild(filterText);
-		 
-	   	// then append it to the select element
-		//appendChild is getting overwritten everytime it's called, that's why data only appears in groupDataStream.
-		document.getElementById('filterDataStream').appendChild(filterOption);
-		
-	};
-	// then append the select to an element in the dom
 
+	for (var i = importedDataSize; i < importedData.length; i++) {
+		text = importedData[i];
+		loadHelper('filterDataStream', text);
+		loadHelper('groupDataStream', text);	
+		loadHelper('aggregateDataStream', text);
+	 	loadHelper('2dCoDataStream1', text);
+	 	loadHelper('2dCoDataStream2', text);
+	 	loadHelper('3dStreams1', text);
+	 	loadHelper('3dStreams2', text);
+	 	loadHelper('3dStreams3', text);
+	};
+	importedDataSize = importedData.length;
+
+	function loadHelper(streamId, text) {
+		var select = document.getElementById(streamId);
+		var option = document.createElement('option');
+		option.text = text;
+		select.add(option);
+	}
+
+	
 }
 
 function Tuple(param1, param2){
@@ -139,6 +154,7 @@ function isValidInput(input){
 	return input !== "";
 }
 
+
 function validateImportData(){
 	var lifeEvents = document.getElementById("lifeEvents");
 	var environmental = document.getElementById("environmental");
@@ -180,6 +196,7 @@ function validateImportData(){
 	}
 	document.getElementById("importClose").click();
 }
+
 
 function validateFilterInput(){
 	//check data stream
@@ -340,9 +357,10 @@ function filterHasErrors(){
 	return false;
 }
 
+
 function validate2DInput(){
-	var dataStream1 = document.getElementById("2dCoDataStream1")
-	var dataStream2 = document.getElementById("2dCoDataStream2");
+	
+	var dataStream2 = document.getElementById("2DdataStream2");
 	if (!isValidInput(dataStream1.value)){
 		dataStream1.parentNode.parentNode.className = "form-group has-error";
 		$(dataStream1).on("change", function(){
@@ -351,7 +369,7 @@ function validate2DInput(){
 			}
 		})
 	}
-	
+
 	if (!isValidInput(dataStream2.value)){
 		dataStream2.parentNode.parentNode.className = "form-group has-error";
 		$(dataStream2).on("change", function(){
@@ -418,10 +436,7 @@ function validateThreeDInput() {
 }
 
 
-
 //  -------------------------    Process Functions   ---------------------
-
-
 
 
 function processFilter(){
@@ -449,13 +464,6 @@ function processFilter(){
 
 		document.getElementById("filterClose").click();
 
-		// $('canvas').drawArc({
-		//   strokeStyle: 'black',
-		//   strokeWidth: 2,
-		//   x: 150, y: 50,
-		//   radius: 30
-		// });
-		// alert("after canvas");
 	} 
 }
 
@@ -470,49 +478,29 @@ function process3D () {
         data = json;
         for (var i = 0; i < data.queries.length; i++) {
         	var inputs = data.queries[i].input;
-        	if (data.queries[i].operator.name === "3Dco_occurrence" && inputs[0] == "ds1" && inputs[1] == "ds2" && inputs[2] == "ds3"){
+        	if (data.queries[i].operator.name === "3Dco_occurrence" && inputs[0] == "es1" && inputs[1] == "es2" && inputs[2] == "es3"){
         		output_path += data.queries[i].output['3dmatrix'];
         	}
         } 
-
-        
-        d3.json(output_path, function(error, json){
-        	if (error) {
-        		return;
-        	}
-        	output_data = json;
-        	for (var i = 0; i < output_data.length; i++) {
-        		for (var j = 0; j < output_data[i].length; j++) {
-        			for (var k = 0; k < output_data[i][j].length; k++) {
-        				HeatMapData.push(output_data[i][j][k]);
-        			};
-        		};
-        	};
-        })
+        processedOperators[4] = true;
+        outputPath3D = output_path;
+		document.getElementById("3DClose").click(); 
     })
-    
-
-    processedOperators[4] = true;
-	document.getElementById("3DClose").click();
 }
 
 
 
 
-//  -------------------------    Visualize Functions   ---------------------
+// Visualize Functions
+
 
 
 function visualize3D () {
 	if (processedOperators[4]){
-		// var heatpmapContainer = create3DImage();
-		// var canvas = heatpmapContainer.childNodes[1];
-		// setup3DBackground(canvas.toDataURL());
-		
-		// $("#heatmapContainerWrapper").remove();
 
 		setup3DBackground();
 
-		render3DBackground();
+		render3DBackground(outputPath3D);
 
 	} else{
 		alert("Fill out the 3D Processor first");
@@ -520,22 +508,17 @@ function visualize3D () {
 }
 
 function setup3DBackground(){
-	var html = "<div id='container'></div><div id='info'><div class='top'>3D co-occurrence Visualization</div><div class='bottom' id='inlineDoc'><div class='controls'><p>Data source:</p><p><select id='datasource'><option value='1x1x1'>1x1x1</option><option value='2x2x2'>2x2x2</option><option value='4x4x4'>4x4x4</option><option value='8x8x8'>8x8x8</option><option value='16x16x16'>16x16x16</option><option value='Sphere'>Sphere</option><option value='I-Shape'>I-Shape</option><option value='Tiny Button'>Tiny Button</option><option value='Noise'>Noise</option><option value='Dense Noise'>Dense Noise</option><option value='16 Color Noise'>16 Color Noise</option><option value='Hole'>Hole</option><option value='Boss'>Boss</option><option value='T-Shape'>T-Shape</option><option value='HollowCube'>HollowCube</option><option value='Clover'>Clover</option><option value='Triangle'>Triangle</option><option value='Saw'>Saw</option><option value='4Dots'>4Dots</option><option value='Checker'>Checker</option><option value='Matt's Example'>Alfonsos Example</option><option value='Benchmark (SLOW!)'>Benchmark (SLOW!)</option><option value='Hill'>Hill</option><option value='Valley'>Valley</option><option value='Hilly Terrain'>Hilly Terrain</option><option value='Empty'>Empty</option></select></p><p>Mesher:</p><p><select id='mesher'><option value='Stupid'>Stupid</option><option value='Culled'>Culled</option><option value='Greedy'>Greedy</option><option value='Monotone'>Monotone</option></select></p><p>Show faces: <input type='checkbox' id='showfacets' value='checked'></p><p>Show edges: <input type='checkbox' id='showedges' value='checked'></p><p>Vertex count: <input type='text' id='vertcount' value='0'> </p><p>Face count: <input type='text' id='facecount' value='0'> </p></div> </div>";
-	var visualizationPanel = document.getElementById("visualizationPanel");
+	var html = "<div id='container' style='margin-left: 200px; display:inline-block;'></div>";
+	var visualizationPanel = document.getElementById("visualizationPanel");	
 	visualizationPanel.innerHTML = html;
 }
 
 
-function render3DBackground () {
+function render3DBackground (path) {
 	var scene, renderer, composer;
     var camera, cameraControl;
     var geometry, surfacemesh, wiremesh;
-    var meshers = {
-        'Stupid': StupidMesh
-      , 'Culled': CulledMesh
-      , 'Greedy': GreedyMesh
-      , 'Monotone': MonotoneMesh
-    };
+    var meshers = { 'Stupid': StupidMesh };
     var testdata = {};
     
     
@@ -545,39 +528,46 @@ function render3DBackground () {
       scene.remove( wiremesh );
       
       geometry  = new THREE.Geometry();   
-      var mesher = meshers[ document.getElementById("mesher").value ]
-        , data   = testdata[ document.getElementById("datasource").value ]
+      var mesher = meshers['Stupid']
+        , data   = testdata['22x22x22']
         , result = mesher( data.voxels, data.dims );
 
-      console.log('result: ' + result.vertices);
-      console.log('faces: ' + result.faces);
-      document.getElementById("vertcount").value = result.vertices.length;
-      document.getElementById("facecount").value = result.faces.length;
       geometry.vertices.length = 0;
       geometry.faces.length = 0;
 
+      
+
+	  // populate cube with vertices
       for(var i=0; i<result.vertices.length; ++i) {
         var q = result.vertices[i];
         geometry.vertices.push(new THREE.Vector3(q[0], q[1], q[2]));
       }
 
+      var yellow = 0xffff00;
+      var orange = 0xff8000;
+      var red = 0xFF0000;
 
+      var jsonData;
+      d3.json(path, function(error, json){
+        if (error) return console.warn(error);
+        jsonData = json;
+        console.log(jsonData[0]);
+        // for (var i = 0; i < jsonData[0].length; i++) {
+        // 	console.log(jsonData[i]);
+        // };
+      });
+
+      // populates colors of faces
       for(var i=0; i<result.faces.length; ++i) {
         var q = result.faces[i];
+        var f = new THREE.Face4(q[0], q[1], q[2], q[3]);
+        // f.color = new THREE.Color(q[4]);
 
-        if(q.length === 5) {
-          var f = new THREE.Face4(q[0], q[1], q[2], q[3]);
-          
-          f.color = new THREE.Color(q[4]);
-          console.log('f - r g b: ' + f.color.r + ', ' + f.color.g + ', ' + f.color.b);
-          f.vertexColors = [f.color,f.color,f.color,f.color];
-          geometry.faces.push(f);
-        } else if(q.length == 4) {
-          var f = new THREE.Face3(q[0], q[1], q[2]);
-          f.color = new THREE.Color(q[3]);
-          f.vertexColors = [f.color,f.color,f.color];
-          geometry.faces.push(f);
-        }
+        f.color = new THREE.Color(yellow);
+
+        f.vertexColors = [f.color,f.color,f.color,f.color];
+        geometry.faces.push(f);
+        
       }
       
       geometry.computeFaceNormals();
@@ -628,46 +618,38 @@ function render3DBackground () {
       }
       renderer.setClearColorHex( 0xffffff, 1 );
       
-      renderer.setSize( 1000, 430 );
+      renderer.setSize( 600, 380 );
       document.getElementById('container').appendChild(renderer.domElement);
 
       // create a scene
       scene = new THREE.Scene();
 
       // put a camera in the scene
-      camera  = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 10000 );
+      camera  = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 10000 );
       camera.position.set(0, 0, 40);
       scene.add(camera);
 
       // create a camera contol
       cameraControls  = new THREE.TrackballControls( camera, document.getElementById('container') )
 
-      // transparently support window resize
-      THREEx.WindowResize.bind(renderer, camera);
-      // allow 'p' to make screenshot
-      THREEx.Screenshot.bindKey(renderer);
-      // allow 'f' to go fullscreen where this feature is supported
-      if( THREEx.FullScreen.available() ){
-        THREEx.FullScreen.bindKey();    
-        document.getElementById('inlineDoc').innerHTML  += "- <i>f</i> for fullscreen";
-      }
+      
 
       
-      //Initialize dom elements
-      testdata = createTestData();
-      var ds = document.getElementById("datasource");
-      for(var id in testdata) {
-        ds.add(new Option(id, id), null);
-      }
-      ds.onchange = updateMesh;
-      var ms = document.getElementById("mesher");
-      for(var alg in meshers) {
-        ms.add(new Option(alg, alg), null);
-      }
-      ms.onchange = updateMesh;
+      // //Initialize dom elements
+      testdata = createCustomTestData();
+      // var ds = document.getElementById("datasource");
+      // for(var id in testdata) {
+      //   ds.add(new Option(id, id), null);
+      // }
+      // ds.onchange = updateMesh;
+
+      // var ms = document.getElementById("mesher");
+      // for(var alg in meshers) {
+      //   ms.add(new Option(alg, alg), null);
+      // }
+      // ms.onchange = updateMesh;
       
-      document.getElementById("showfacets").checked = true;
-      document.getElementById("showedges").checked  = true;
+      
       
       //Update mesh
       updateMesh();
@@ -675,13 +657,34 @@ function render3DBackground () {
       return false;
     }
 
+    function createCustomTestData() {
+	  var result = {};
+	  
+	  function makeVoxels(l, h, f) {
+	    var d = [ h[0]-l[0], h[1]-l[1], h[2]-l[2] ]
+	      , v = new Int32Array(d[0]*d[1]*d[2])
+	      , n = 0;
+	    for(var k=l[2]; k<h[2]; ++k)
+	    for(var j=l[1]; j<h[1]; ++j)
+	    for(var i=l[0]; i<h[0]; ++i, ++n) {
+	      v[n] = f(i,j,k);
+	    }
+	    return {voxels:v, dims:d};
+	  }
+
+	  result['22x22x22'] = makeVoxels([0,0,0], [22,22,22], function() { return 0xffff00; });
+
+	  return result;
+	}
+
+
     // animation loop
     function animate() {
 
       // loop on request animation loop
       // - it has to be at the begining of the function
       // - see details at http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-      requestAnimationFrame( animate );
+      requestAnimationFrame(animate);
 
       // do the render
       render();
@@ -695,10 +698,13 @@ function render3DBackground () {
       // update camera controls
       cameraControls.update();
 
-      surfacemesh.visible = document.getElementById("showfacets").checked;
-      wiremesh.visible = document.getElementById("showedges").checked;
+      // surfacemesh.visible = document.getElementById("showfacets").checked;
+      // wiremesh.visible = document.getElementById("showedges").checked;
+      surfacemesh.visible = true;
+      wiremesh.visible = true;
 
       // actually render the scene
       renderer.render( scene, camera );
     }
 }
+
