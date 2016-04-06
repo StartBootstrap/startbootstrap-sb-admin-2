568b08940dec560b2390f061f60af5a791cc5ee3
Bobby Shannon <rshannon@buffalo.edu>
2016-04-06 16:30:00 -0400

    Add Gruntfile

--- bower_components/datatables-responsive/make.sh
+++ bower_components/datatables-responsive/make.sh
@@ -11,8 +11,8 @@ DT_BUILT="${DT_SRC}/built/DataTables"
 . $DT_SRC/build/include.sh
 
 # Copy CSS
+scss_compile css/dataTables.responsive.scss
 rsync -r css $OUT_DIR
-css_frameworks responsive $OUT_DIR/css
 
 # Copy images
 #rsync -r images $OUT_DIR
@@ -20,7 +20,6 @@ css_frameworks responsive $OUT_DIR/css
 # Copy JS
 rsync -r js $OUT_DIR
 js_compress $OUT_DIR/js/dataTables.responsive.js
-js_frameworks responsive $OUT_DIR/js
 
 # Copy and build examples
 rsync -r examples $OUT_DIR
