fdc828feec664c01734ed65ea4b08a7edc95cda5
suraymittal <suraymittal@users.noreply.github.com>
2019-09-27 20:59:21 +0530

    Version 1
    Suray Mittal

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -6,7 +6,7 @@
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
+      $(".sidebar .collapse").collapse("hide");
     };
   });
 
