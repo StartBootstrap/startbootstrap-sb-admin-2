f8224ed0640a76f298d7073fc4fcdcfc11aa809a
Riccardo Eudizi <r.eudizi@infos.it>
2019-11-17 00:39:00 +0100

    Edit Chart's colors  and labels

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Petrolio", "Micro-Plastica", "Materiali radioattivi"],
     datasets: [{
       data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      backgroundColor: ['#000000','#116600','#FFBF00'],
+      hoverBackgroundColor: ['#000000','#116600','#FFBF00'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
